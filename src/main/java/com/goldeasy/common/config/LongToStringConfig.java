d0cd4b804f59bcc1371716177c17f3f5a6855fee
wgy <Administrator@USER-20170627QP>
2018-10-15 17:15:56 +0800

    common添加long 转换为string的配置类

---
+++ src/main/java/com/goldeasy/common/config/LongToStringConfig.java
@@ -0,0 +1,35 @@
+package com.goldeasy.common.config;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+/**
+ * @Auther: wgy
+ * @Date: 2018/10/15 17:00
+ * @Description: long转换为string的转换器，主要是long类型返回前端的时候，可能损失精度，转换为string可以解决此问题
+ */
+@Configuration
+public class LongToStringConfig implements WebMvcConfigurer {
+
+
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        MappingJackson2HttpMessageConverter jackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
+        ObjectMapper objectMapper = jackson2HttpMessageConverter.getObjectMapper();
+
+        SimpleModule simpleModule = new SimpleModule();
+        simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
+        simpleModule.addSerializer(Long.TYPE, ToStringSerializer.instance);
+        objectMapper.registerModule(simpleModule);
+
+        jackson2HttpMessageConverter.setObjectMapper(objectMapper);
+        converters.add(0, jackson2HttpMessageConverter);
+    }
+}
