6c47c60b0fb90ba92bba0a3c51205ea7b4355474
wujianan <wjn@puhuilicai.com>
2018-10-22 17:27:42 +0800

    获取行情util

---
+++ src/main/java/com/goldeasy/common/market/MarketUtil.java
@@ -0,0 +1,31 @@
+package com.goldeasy.common.market;
+
+import com.alibaba.fastjson.JSON;
+import com.goldeasy.common.redis.RedisService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * @Author: Mr.wu
+ * @Created time : 2018/10/22 16:52
+ * @Description :
+ */
+@Component
+public final class MarketUtil  {
+
+    @Resource
+    private RedisService redisService;
+
+    @Value("${gold.latest.market}")
+    private  String goldLatestMarket;
+    public Market getMarket(){
+        String marketStr = redisService.get(goldLatestMarket);
+        return JSON.parseObject(marketStr, Market.class);
+    }
+
+
+}
