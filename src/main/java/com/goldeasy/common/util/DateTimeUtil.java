9a1b624c6f73ebff29b7826f89d1e1fe873e8bfd
wgy <Administrator@USER-20170627QP>
2018-10-22 16:23:21 +0800

    localDateTime和Date的相互转换类

---
+++ src/main/java/com/goldeasy/common/util/DateTimeUtil.java
@@ -0,0 +1,39 @@
+package com.goldeasy.common.util;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+/**
+ * 日期相关的工具类
+ */
+public class DateTimeUtil {
+    /**
+     * jdk8时间转换为Date
+     * @param localDateTime jdk8 时间
+     * @return
+     */
+    public static Date toDate(LocalDateTime localDateTime){
+        return new Date().from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+    /**
+     *  jdk8时间转换为Date
+     * @param localDate jdk8时间
+     * @return
+     */
+    public static Date toDate(LocalDate localDate){
+        return new Date().from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+    }
+
+    /**
+     * Date转换为jdk8时间
+     * @param date
+     * @return
+     */
+    public static LocalDateTime toLocalDateTime(Date date){
+       return LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
+    }
+}
