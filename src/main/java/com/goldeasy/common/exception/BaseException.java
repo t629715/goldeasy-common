357ad8a9a9456c91b914c75573b68e4af8eb4c70
xiejunxing <350757337@qq.com>
2018-10-23 14:41:10 +0800

    common中添加BaseException

---
+++ src/main/java/com/goldeasy/common/exception/BaseException.java
@@ -0,0 +1,27 @@
+package com.goldeasy.common.exception;
+
+/**
+ * Created by Administrator on 2018/10/22.
+ * @author xiejunxing
+ */
+public class BaseException extends RuntimeException{
+    public BaseException() {
+    }
+
+    public BaseException(String message) {
+        super(message);
+    }
+
+    public BaseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public BaseException(Throwable cause) {
+        super(cause);
+    }
+
+    public BaseException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+}
